d5ac777bf11ec5b108220d6d1aab943e826ac5fa
aksam <aksam.iftikhar@gmail.com>
2023-09-04 14:24:58 +0500

    HelloWorld Commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
-        int x = 786;
-        System.out.println("Bismillah!" + x);
+        System.out.print("Hello ");
+        System.out.println("World");
+        System.out.print("!");
     }
 }
