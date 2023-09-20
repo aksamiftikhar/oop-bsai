e905c299b9493b6ea33b4064e5b96eaf695ac630
aksam <aksam.iftikhar@gmail.com>
2023-09-20 13:28:37 +0500

    Deep copy vs Shallow Copy

--- src/Lab3/Vehicle.java
+++ src/Lab3/Vehicle.java
@@ -35,34 +35,47 @@ public class Vehicle {
         return this;
     }
     public boolean equals(Vehicle vehicle2) {
-//        if (this == vehicle2) return true;
+//        if (this == vehicle2) return true
+//        if (!brand.equals(vehicle2.brand)) return false;
+//        else if (!model.equals(vehicle2.model)) return false;
+//        else return true;
 
-        if (year != vehicle2.year) return false;
         if (!brand.equals(vehicle2.brand)) return false;
         return model.equals(vehicle2.model);
     }
 }
 class VehicleTest {
     public static void main(String[] args) {
-        // Experiment 1 Test code
-        // Create vehicles using different constructors
-        Vehicle v1 = new Vehicle("Toyota");
-        Vehicle v2 = new Vehicle("Honda", "Civic");
-        Vehicle v3 = new Vehicle("Ford", "Mustang", 2020);
-        Vehicle v4 = v3.makeCopy();
-        v4.brand = "My brand";
-
-
-        // Display the details of each vehicle
-//        v1.displayVehicleInfo();
-//        v2.displayVehicleInfo();
-        v3.displayVehicleInfo();
-        v4.displayVehicleInfo();
-        // Experiment 3 Test code
-//        Vehicle car1 = new Vehicle("Toyota", "Camry", 2020);
-//        Vehicle car2 = new Vehicle("Toyota", "Camry", 2020);
-//        Vehicle car3 = new Vehicle("Honda", "Accord", 2019);
+//        // Experiment 1 Test code
+//        // Create vehicles using different constructors
+//        Vehicle v1 = new Vehicle("Toyota");
+//        Vehicle v2 = new Vehicle("Honda", "Civic");
+//        Vehicle v3 = new Vehicle("Ford", "Mustang", 2020);
+//        Vehicle v4 = v3.makeCopy();
 //
+//
+//        v4.brand = "My brand";
+//
+//
+//        // Display the details of each vehicle
+////        v1.displayVehicleInfo();
+////        v2.displayVehicleInfo();
+//        v3.displayVehicleInfo();
+//        v4.displayVehicleInfo();
+//         Experiment 3 Test code
+        Vehicle car1 = new Vehicle("Toyota", "Camry", 2020);
+        Vehicle car2 = new Vehicle("Toyota", "Camry", 2020);
+        Vehicle car3 = new Vehicle("Honda", "Accord", 2019);
+        if(car1==car2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not equal");
+
+        if(car1.equals(car2))
+            System.out.println("Equal");
+        else
+            System.out.println("Not equal");
+
 //        // Check if two vehicles are equal using the overridden equals method
 //        System.out.println("Are car1 and car2 equal? " + car1.equals(car2));  // Expected: true
 //        System.out.println("Are car1 and car3 equal? " + car1.equals(car3));  // Expected: false
